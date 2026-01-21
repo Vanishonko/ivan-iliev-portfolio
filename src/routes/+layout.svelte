82986a6efcc0cb709307e323ee1c55b6eb2c8df7
Ivan Iliev <ilie0012@hz.nl>
2026-01-21 03:22:07 +0100

    changed title font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Press+Start+2P&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
